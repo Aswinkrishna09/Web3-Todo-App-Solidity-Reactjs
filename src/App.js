a410021cc43e5c8dc7db6edfcd29b27b42edad20
Aswin krishna <aswinkts57@gmail.com>
2022-11-13 12:17:33 +0530

    commit before push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Alert, Button, Col, Row } from "antd";
+import React, { useEffect, useState } from "react";
+import Todo from "./Todo";
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isCorrectNetwork, setIsCorrectNetwork] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Metamask is not detected");
+        return;
+      }
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      console.log("connected to chain ", chainId);
+
+      const goerliChainId = "0x5";
+      if (chainId !== goerliChainId) {
+        setIsCorrectNetwork(false);
+        alert("You are not connected with Goerli Chain");
+        return;
+      } else {
+        setIsCorrectNetwork(true);
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Found account ", accounts[0]);
+      setIsLoggedIn(true);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    connectWallet();
+    console.log("connecting.....");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row style={{ margin: "10px" }} justify="center">
+      {isLoggedIn ? (
+        isCorrectNetwork ? (
+          <Todo />
+        ) : (
+          <Alert
+            message="Error"
+            description="Wrong Network"
+            type="error"
+            showIcon
+          />
+        )
+      ) : (
+        <Col span={16}>
+          <Button onClick={connectWallet} size="large">
+            Connect Wallet
+          </Button>
+        </Col>
+      )}
+    </Row>
   );
-}
+};
 
 export default App;
